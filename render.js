270367b45a9347e92e64a7233b8b859f320bcda5
Daniel Willemse <daniel.willemse@holder.nl>
2015-11-23 08:24:03 +0100

    Add level rendering

--- render.js
+++ render.js
@@ -1,18 +1,30 @@
 var render = function() {
   renderWorld();
+  renderLevel();
   renderHero();
 }
 
 var renderWorld = function() {
-  ctx = window.game.ctx;
+  var ctx = window.game.ctx;
 
   ctx.fillStyle = "#ffffff";
   ctx.fillRect(0,0,600,600);
 }
 
+var renderLevel = function() {
+  var ctx = window.game.ctx;
+  var level = window.game.level;
+
+  for (index in level.grid) {
+    var node = level.grid[index];
+    ctx.fillStyle = "#ff6600";
+    ctx.fillRect(node.x,node.y,15,15);
+  }
+}
+
 var renderHero = function() {
-  ctx = window.game.ctx;
-  hero = window.game.hero;
+  var ctx = window.game.ctx;
+  var hero = window.game.hero;
 
   ctx.fillStyle = "#000000";
   ctx.fillRect(hero.x,hero.y,15,15);
--- world.js
+++ world.js
@@ -8,7 +8,8 @@ var loadGame = function() {
 
   return {
     ctx: ctx,
-    hero: getHero()
+    hero: getHero(),
+    level: getLevel()
   }
 }
 
@@ -16,5 +17,22 @@ var getHero = function() {
   return {
     x: 0,
     y: 0
+  }
+}
+
+var getLevel = function() {
+  return {
+    grid: [
+      {x: 105, y: 105, type: 'wall'},
+      {x: 120, y: 105, type: 'wall'},
+      {x: 135, y: 105, type: 'wall'},
+      {x: 150, y: 105, type: 'wall'},
+      {x: 150, y: 120, type: 'wall'},
+      {x: 150, y: 135, type: 'wall'},
+      {x: 150, y: 150, type: 'wall'},
+      {x: 150, y: 165, type: 'wall'},
+      {x: 150, y: 180, type: 'wall'},
+      {x: 150, y: 195, type: 'wall'},
+    ]
   }
 }
