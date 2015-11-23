438abd1eda8186ef2f4e21895941fbc2946bbb27
Daniel Willemse <daniel.willemse@holder.nl>
2015-11-23 08:49:14 +0100

    Add level collision

--- updater.js
+++ updater.js
@@ -5,33 +5,47 @@ var update = function () {
 var updateHero = function() {
   var modifier = 15;
   var hero = window.game.hero;
+  var x_mod = 0;
+  var y_mod = 0;
 
-  if (38 in keysDown) { // Player holding up
-    hero.y -= modifier;
+  if (38 in keysDown) { // Up
+    y_mod = modifier * -1;
   }
-  if (40 in keysDown) { // Player holding down
-    hero.y += modifier;
+  if (40 in keysDown) { // Down
+    y_mod = modifier;
   }
-  if (37 in keysDown) { // Player holding left
-    hero.x -= modifier;
+  if (37 in keysDown) { // Left
+    x_mod = modifier * -1;
   }
-  if (39 in keysDown) { // Player holding right
-    hero.x += modifier;
-  }
-
-  if (hero.x < 0) {
-    hero.x = 0;
+  if (39 in keysDown) { // Right
+    x_mod = modifier;
   }
 
-  if (hero.y < 0) {
-    hero.y = 0;
+  if (validHeroMove(hero.x + x_mod, hero.y + y_mod)) {
+    hero.x += x_mod;
+    hero.y += y_mod;
   }
 
-  if (hero.x + 15 > 600) {
-    hero.x = 600 - 15;
+}
+
+var validHeroMove = function(new_x, new_y) {
+  return (
+    (new_x >= 0 && new_x <= (600 - 15)) &&
+    (new_y >= 0 && new_y <= (600 - 15)) &&
+    notWallCollision(new_x, new_y)
+  );
+}
+
+var notWallCollision = function(new_x, new_y) {
+  var level = window.game.level;
+
+  for (index in level.grid) {
+    var node = level.grid[index];
+
+    if (new_x == node.x && new_y == node.y) {
+      return false;
+    }
   }
 
-  if (hero.y + 15 > 600) {
-    hero.y = 600 - 15;
-  }
+  return true;
 }
