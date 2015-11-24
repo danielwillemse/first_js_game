d38e32e094f0cd687833b47a454044f179846a2d
Daniel Willemse <daniel.willemse@holder.nl>
2015-11-24 08:51:43 +0100

    Use node dimensions to render hero and nodes

--- render.js
+++ render.js
@@ -13,11 +13,11 @@ var renderLevel = function() {
   for (index in Game.level.grid) {
     var node = Game.level.grid[index];
     Game.ctx.fillStyle = "#ff6600";
-    Game.ctx.fillRect(node.x,node.y,15,15);
+    Game.ctx.fillRect(node.x,node.y,Game.world.node_width,Game.world.node_height);
   }
 }
 
 var renderHero = function() {
   Game.ctx.fillStyle = "#000000";
-  Game.ctx.fillRect(Game.hero.x,Game.hero.y,15,15);
+  Game.ctx.fillRect(Game.hero.x,Game.hero.y,Game.world.node_width,Game.world.node_width);
 }
