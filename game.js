5b8b281d64436f4a78079ecc1d45c43f9814f181
Daniel Willemse <daniel.willemse@holder.nl>
2015-11-25 22:48:37 +0100

    Prevent key defaults on press

--- game.js
+++ game.js
@@ -15,10 +15,12 @@ addKeyListeners = function() {
   Game.keysDown = {};
 
   addEventListener("keydown", function (e) {
+    e.preventDefault();
     Game.keysDown[e.keyCode] = true;
   }, false);
 
   addEventListener("keyup", function (e) {
+    e.preventDefault();
     delete Game.keysDown[e.keyCode];
   }, false);
 }
